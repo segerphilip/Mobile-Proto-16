4bd2946bfe2be315c0b2ec8f65ebfec90c2b2caa
Philip Seger <seger.philip@gmail.com>
2016-09-14 21:40:57 -0400

    gets back to after inclass

---
+++ lesson3/Lesson3HW/app/src/main/java/pip/lesson3hw/MainActivityFragment.java
@@ -0,0 +1,45 @@
+package pip.lesson3hw;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        Button myButton1 = (Button) view.findViewById(R.id.fragmentButton);
+        Button myButton2 = (Button) view.findViewById(R.id.fragmentButtonClick);
+        final TextView text = (TextView) view.findViewById(R.id.fragmentTextView);
+
+        myButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("MainActivityFragment", "Boring print statements in logs are boring");
+                text.setText("So long suckas");
+            }
+        });
+
+        myButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Hello agaWAIT WHAT?! I\'M STUCK IN AN APP!!!");
+            }
+        });
+
+        return view;
+    }
+}

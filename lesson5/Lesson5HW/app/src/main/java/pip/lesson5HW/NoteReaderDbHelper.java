c4c4ac3f4b9fb1fa13e8801bc537f4e07c595ddb
segerphilip <seger.philip@gmail.com>
2016-09-27 11:36:08 -0400

    includes more hw5 progress and starts hw6

--- lesson5/Lesson5HW/app/src/main/java/pip/lesson5HW/NoteReaderDbHelper.java
+++ lesson5/Lesson5HW/app/src/main/java/pip/lesson5HW/NoteReaderDbHelper.java
@@ -10,12 +10,13 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class NoteReaderDbHelper extends SQLiteOpenHelper {
     // defs for standard db operations
     private static final String TEXT_TYPE = " TEXT";
+    private static final String INT_TYPE = " INT";
     private static final String COMMA_SEP = ",";
     private static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + NoteReaderContract.NoteEntry.TABLE_NAME + " (" +
                     NoteReaderContract.NoteEntry._ID + " INTEGER PRIMARY KEY," +
                     NoteReaderContract.NoteEntry.COLUMN_NAME_NOTE + TEXT_TYPE + COMMA_SEP +
-                    NoteReaderContract.NoteEntry.COLUMN_NAME_DONE + TEXT_TYPE + " )";
+                    NoteReaderContract.NoteEntry.COLUMN_NAME_DONE + INT_TYPE + " )";
     private static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + NoteReaderContract.NoteEntry.TABLE_NAME;
 

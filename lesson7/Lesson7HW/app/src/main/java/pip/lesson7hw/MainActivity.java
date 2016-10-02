9dba7c94a996d4189ec4f1de12af4e38ff30b91d
Philip Seger <seger.philip@gmail.com>
2016-10-02 14:47:30 -0400

    adds no new functionality, but it looks good

--- lesson7/Lesson7HW/app/src/main/java/pip/lesson7hw/MainActivity.java
+++ lesson7/Lesson7HW/app/src/main/java/pip/lesson7hw/MainActivity.java
@@ -2,13 +2,11 @@ package pip.lesson7hw;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,39 +17,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        // create to-do items fragment and add it to current activity
-        final MainActivityFragment newFragment = new MainActivityFragment();
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragment, newFragment, "activityFragment");
-        transaction.commit();
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // use fab to call search new ticker in fragment
+                MainActivityFragment fragment = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+                fragment.searchTicker();
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- lesson7/Lesson7HW/app/src/main/java/pip/lesson7hw/MainActivityFragment.java
+++ lesson7/Lesson7HW/app/src/main/java/pip/lesson7hw/MainActivityFragment.java
@@ -1,15 +1,52 @@
 package pip.lesson7hw;
 
-import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import org.json.JSONException;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    private final String TAG = this.getClass().getName();
+
+    private Response.Listener<String> responseListener = new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+            // YOUR CODE HERE. DO SOMETHING WHEN A RESPONSE COMES IN.
+            // Hint: remove the first three characters, parse the response into a JSONArray,
+            // and pass it into your extractPriceFromJSON() function.
+        }
+    };
+
+    private Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.e(TAG, "A VolleyError occurred.");
+            error.printStackTrace();
+        }
+    };
 
     public MainActivityFragment() {
     }
@@ -17,6 +54,51 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        final Context c = this.getContext();
+
+                // YOUR CODE HERE.
+                //
+                // Create a StringRequest using the URL and the listeners declared above.
+                // Add the request to your RequestQueue from your MySingleton class
+
+        return view;
+    }
+
+    private String buildSearchURL(String companyTicker) {
+        // YOUR CODE HERE
+        // USE URIBuilder
+        return "";
+    }
+
+    private String extractPriceFromJSON(JSONArray array) throws JSONException {
+        // Your code here. Extract the price value from the JSON array
+        return "";
+    }
+
+    /**
+     * Displays alert dialog for user to insert a ticker name, called from activity fab button.
+     */
+    public void searchTicker() {
+        final EditText input = new EditText(getContext());
+        // set focus and popup keyboard
+        input.setSelectAllOnFocus(true);
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(getContext())
+                .setMessage(R.string.enter_ticker_here)
+                .setView(input)
+                .setCancelable(true)
+                .setPositiveButton(R.string.confirm_text_input, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String task = String.valueOf(input.getText().toString());
+                    }
+                });
+
+        AlertDialog visibleAlert = alert.create();
+        // popup the soft keyboard when the alert dialog opens, close when alert dialog closes
+        visibleAlert.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        visibleAlert.show();
     }
 }
